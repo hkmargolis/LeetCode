92d8d639cbc62583d4ffba67985384ff27dae85b
Hannah <hkmargolis@gmail.com>
2024-08-09 19:03:48 -0700

    Adds maxSubArray()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        //---------------------------------------------------
         boolean isAnagram = validAnagram("rat", "tar");
         System.out.println("validAnagram(rat, tar): " + isAnagram);
         isAnagram = validAnagram("rat", "cat");
         System.out.println("validAnagram(rat, cat): " + isAnagram);
-
+        //---------------------------------------------------
         int[] twoSumSolution = twoSum(new int[]{2,1,5,3}, 4);
         System.out.println("twoSum({2,1,5,3}, 4): " + "indices [" +  twoSumSolution[0] + ", " + twoSumSolution[1] + "]");
-
+        //---------------------------------------------------
+        int maxSubArray = maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        System.out.println("maxSubArray({-2,1,-3,4,-1,2,1,-5,4}) sum: " + maxSubArray);
     }
 
     /**
@@ -20,9 +23,6 @@ public class Main {
      * @return true if valid anagram, else false
      */
     public static boolean validAnagram(String s, String t) {
-        //example: rat, tar == true
-        //example: rat, cat == false
-
         //method 1---------------------------------------------------
         //time complexity: O(s+t)
         //memory complexity: O(s+t)
@@ -80,6 +80,12 @@ public class Main {
 
     }
 
+    /**
+     * method solves two sum problem
+     * @param values an integer array
+     * @param target an integer value
+     * @return an integer array of two indices
+     */
     public static int[] twoSum(int[] values, int target) {
         //given an array of integers, return indices of the two numbers such that they add up to a certain target
         //can assume exactly one solution
@@ -99,6 +105,42 @@ public class Main {
             }
         }
         return null;
+    }
+
+    /**
+     * method returns sum of max sub array of integers
+     * @param array of integers
+     * @return integer sum
+     */
+    public static int maxSubArray(int[] array) {
+        //find the contiguous subarray that has the largest sum
+        //can assume array is not empty
+
+        //method 1---------------------------------------------------
+        //time complexity: O(n^3) 3 for loops
+        //for loop i (start of array)
+        //nested for loop j (end of array)
+        //nested for loop k (i-->j) sums subarray
+
+        //method 2---------------------------------------------------
+        //time complexity: O(n^2)
+        //for loop i (start of array)
+        //nested for loop j (end of array) within this loop add j to sum
+
+        //method 3---------------------------------------------------
+        //time complexity: O(n)
+        //"sliding window" problem
+        int maxSub = array[0];
+        int sum = 0;
+        for(int num : array) {
+            if(sum < 0) {
+                sum = 0;
+            }
+            sum += num;
+            if(maxSub < sum) {
+                maxSub = sum;
+            }
+        }return maxSub;
     }
 
 }
